c6fd16d68fff9a965295afccc68d997a91eb1503
Duncan Moyo <duncanfmoyo@gmail.com>
2020-03-02 04:11:52 +0200

    using switch-case statements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -33,41 +33,85 @@ function writeToLog(event, value, monsterHealth, playerHealth) {
     finalMonsterHealth: monsterHealth,
     finalPlayerHealth: playerHealth
   };
-  // you can just specify the target to save even more code, but will leae it like this for now
-  if (event === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry.target = "MONSTER";
-  } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry = {
-      event: event,
-      value: value,
-      target: "MONSTER",
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    };
-  } else if (event === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry = {
-      event: event,
-      value: value,
-      target: "PLAYER",
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    };
-  } else if (event === LOG_EVENT_PLAYER_HEAL) {
-    logEntry = {
-      event: event,
-      value: value,
-      target: "PLAYER",
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    };
-  } else if (event === LOG_EVENT_GAME_OVER) {
-    logEntry = {
-      event: event,
-      value: value,
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth
-    };
+  switch (event) {
+    case LOG_EVENT_PLAYER_ATTACK:
+        logEntry.target = "MONSTER";
+        break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+        logEntry = {
+            event: event,
+            value: value,
+            target: "MONSTER",
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+        break;
+    case LOG_EVENT_MONSTER_ATTACK:
+        logEntry = {
+            event: event,
+            value: value,
+            target: "PLAYER",
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+        break;
+    case LOG_EVENT_PLAYER_HEAL:
+        logEntry = {
+            event: event,
+            value: value,
+            target: "PLAYER",
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+        break;
+    case LOG_EVENT_GAME_OVER:
+        logEntry = {
+            event: event,
+            value: value,
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+    default:
+        logEntry = {};
   }
+
+// OR
+
+  // you can just specify the target to save even more code, but will leae it like this for now
+//   if (event === LOG_EVENT_PLAYER_ATTACK) {
+//     logEntry.target = "MONSTER";
+//   } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+//     logEntry = {
+//       event: event,
+//       value: value,
+//       target: "MONSTER",
+//       finalMonsterHealth: monsterHealth,
+//       finalPlayerHealth: playerHealth
+//     };
+//   } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+//     logEntry = {
+//       event: event,
+//       value: value,
+//       target: "PLAYER",
+//       finalMonsterHealth: monsterHealth,
+//       finalPlayerHealth: playerHealth
+//     };
+//   } else if (event === LOG_EVENT_PLAYER_HEAL) {
+//     logEntry = {
+//       event: event,
+//       value: value,
+//       target: "PLAYER",
+//       finalMonsterHealth: monsterHealth,
+//       finalPlayerHealth: playerHealth
+//     };
+//   } else if (event === LOG_EVENT_GAME_OVER) {
+//     logEntry = {
+//       event: event,
+//       value: value,
+//       finalMonsterHealth: monsterHealth,
+//       finalPlayerHealth: playerHealth
+//     };
+//   }
   battleLog.push(logEntry);
 }
 
