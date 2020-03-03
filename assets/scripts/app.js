4966c5a198c2af1ef3ff157f139f3a5acfadde07
Duncan Moyo <duncanfmoyo@gmail.com>
2020-03-03 04:29:15 +0200

    for-in loops

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -228,8 +228,14 @@ function printLogHandler() {
   for (let i = 0; i < battleLog.length; i++) {
       console.log(battleLog[i])
   }
+  let i = 0;
   for (const logEntry of battleLog) {
-      console.log(logEntry);
+      console.log(`#${i}`);
+      for (const key in logEntry) {
+          console.log(key);
+          console.log(`${key} => ${logEntry[key]}`);
+      } 
+      i++;
   }
 }
 
