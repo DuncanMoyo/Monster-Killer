8c82f179412eb3aee201212b40219d0c719b5da1
Duncan Moyo <duncanfmoyo@gmail.com>
2020-03-04 22:23:15 +0200

    introducing black scope

---
+++ app.js
@@ -0,0 +1,14 @@
+let name = 'Duncan';
+
+if (name === 'Duncan') {
+    let hobbies = ['Sports', 'Cooking'];
+    console.log(hobbies);
+}
+
+function greet() {
+    let age = 28;
+    let name = 'Duncan_Moyo'
+    console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
