76a2822b75acc9da8ba75477434dd17b03edd65b
Duncan Moyo <duncanfmoyo@gmail.com>
2020-03-10 03:09:27 +0200

    anonymous functions

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function() {
-  console.log('Game is starting...'); 
-};
+// const start = function() {
+//   console.log('Game is starting...'); 
+// };
 
 // const person = {
 //   name: 'Duncan',
@@ -15,4 +15,6 @@ const start = function() {
 
 console.log(typeof startGame); 
 
-startGameBtn.addEventListener('click', start);
+startGameBtn.addEventListener('click', function() {
+  console.log('Game is starting...'); 
+});
